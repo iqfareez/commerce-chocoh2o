c98f1e295d4ae270fdd90edb4dd9023a712fe217
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-03-30 10:11:21 +0800

    buang biskut

--- site/lib/hooks/useAcceptCookies.ts
+++ site/lib/hooks/useAcceptCookies.ts
@@ -7,9 +7,7 @@ export const useAcceptCookies = () => {
   const [acceptedCookies, setAcceptedCookies] = useState(true)
 
   useEffect(() => {
-    if (!Cookies.get(COOKIE_NAME)) {
-      setAcceptedCookies(false)
-    }
+
   }, [])
 
   const acceptCookies = () => {
